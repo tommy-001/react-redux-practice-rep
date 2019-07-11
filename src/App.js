19ead9faff1cd838221d50cff5a77b4197ab889e
tommy-001 <tomita.kohei.work@gmail.com>
2019-07-11 17:31:37 +0900

    jsxでできること

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+// JSXのタグはトランスパイルをするとReact.create.elementに変換されるので必ず必要
 import React from 'react';
 
 function App() {
+  // 変数を返却することもできる
+  //const test = "Hi!, Tommy"
+  //class名を定義するためにはclassNameとする
+  //const dom = <h1 className="foo">{test}</h1>;
+  // return dom
+
+  //returnで返すタブは1つでなければならないのでdevで囲う
+  //React.Fragmentを使うことで不要なdevを使わなくてもいい
   return (
-    <div>
-      <h1>Hello, World</h1>
-    </div>
-  );
+    <React.Fragment>
+      <label htmlFor="bar">
+        bar
+      </label>
+      <input type="text" onClick={() => {console.log("I am clicked")}} />
+    </React.Fragment>
+  )
 }
+
 
 export default App;
