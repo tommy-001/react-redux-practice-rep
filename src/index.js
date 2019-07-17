54316bd69edbd7eabc9fd8f8040dbcf6fb69aaff
tommy-001 <tomita.kohei.work@gmail.com>
2019-07-17 10:50:36 +0900

    一覧画面にマテリアルUI実装

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools} from 'redux-devtools-extension'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import reducer from './reducers'
@@ -25,16 +26,18 @@ const store = createStore(reducer, enhanser)
 AppをProviderでラップしてそこでstoreを渡す
 */
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route path="/events/new" component={EventsNew} />
-                <Route path="/events/:id" component={EventsShow} />
-                <Route path="/" component={EventsIndex} />
-                <Route path="/events" component={EventsIndex} />
-            </Switch>
-        </BrowserRouter>
-    </Provider>,
+    <MuiThemeProvider>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/events/new" component={EventsNew} />
+                    <Route path="/events/:id" component={EventsShow} />
+                    <Route path="/" component={EventsIndex} />
+                    <Route path="/events" component={EventsIndex} />
+                </Switch>
+            </BrowserRouter>
+        </Provider>
+    </MuiThemeProvider>,
     document.getElementById('root')
 );
 
